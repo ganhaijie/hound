5db0eb2c9fbb387dfddc3d91128849bff2a77ada
ganhaijie <ganhaijie1028@gmail.com>
2018-01-08 10:18:11 +0800

    xx

--- hound-common/src/main/java/com/ghj/hound/common/config/TLProperties.java
+++ hound-common/src/main/java/com/ghj/hound/common/config/TLProperties.java
@@ -1,6 +1,13 @@
 package com.ghj.hound.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 public class TLProperties {
@@ -10,6 +17,24 @@ public class TLProperties {
     public static String TL_APIKEY;
 
     static {
-            
+        FileInputStream in = null;
+        try {
+            File file = ResourceUtils.getFile("classpath:file.properties");
+            Properties p = new Properties();
+            in = new FileInputStream(file);
+            p.load(in);
+            TL_APIKEY = p.getProperty("mediation.file.inputFile");
+            TL_APIADDRESS = p.getProperty("mediation.file.finishedFile");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (in != null) try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
